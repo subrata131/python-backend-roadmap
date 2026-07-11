a9bc29748b7abba6a683b5af7912a16541266e53
Subrata Das <bikashdas991915@gmail.com>
2026-07-11 11:07:21 +0530

    Add files via upload

---
+++ Day-02/Home_Work1.py
@@ -0,0 +1,6 @@
+n=int(input("Enter your Age:"))
+if n>=18:
+    print("You are eligible for voting")
+else:
+    print("You are not eligible for voting")
+    
---
+++ Day-02/Number_Guess(ver1).py
@@ -0,0 +1,9 @@
+n=5
+while n>0:
+    g=int(input("Guess The number:"))
+    if g==5:
+        print("Congratulations! You Gussed it!")
+        break
+    else:
+        print("Try Again")
+     
