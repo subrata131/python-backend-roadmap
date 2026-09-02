ae583e2363524ac832a5409eb7f2a332c73ad8bf
Subrata Das <bikashdas991915@gmail.com>
2026-09-02 23:32:59 +0530

    Add circle and rectangle classes with area method

---
+++ Day-14/task4.py
@@ -0,0 +1,10 @@
+class circle:
+    def area(self):
+        print("Area of circle is pi*r^2")
+
+class rectangle:
+    def area(self):
+        print("Area of rectangle is l*b")
+
+def calculate_area(shape):
+    shape.area()
